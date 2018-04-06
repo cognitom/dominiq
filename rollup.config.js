c7636fd203fe7f63871d43b4af38ad9b7eb654b1
Tsutomu Kawamura <cognitom@gmail.com>
2018-04-06 17:30:54 +0000

    Remove uglify temporary (uglify doesn't handle async generator)

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,12 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
-import uglify from 'rollup-plugin-uglify'
 
 const production = !process.env.ROLLUP_WATCH
 
 export default {
   plugins: [
     resolve(),
-    commonjs(),
-    production && uglify()
+    commonjs()
   ],
   output: {
     format: 'umd',
