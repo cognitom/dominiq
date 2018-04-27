f03d3439bc97bd1edd122b85501ed470d54c3120
Tsutomu Kawamura <cognitom@gmail.com>
2018-04-27 15:33:24 +0000

    Adds observable polyfill script

---
+++ polyfill/rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
+
+export default {
+  input: 'polyfill/observable.js',
+  plugins: [
+    resolve(),
+    commonjs(),
+    uglify()
+  ],
+  output: {
+    file: 'observable-polyfill.js',
+    format: 'iife',
+    name: 'Observable',
+    sourcemap: false
+  }
+}
