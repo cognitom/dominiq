3e6d955670ab22d6c0f4fdb444610f4f58018979
Tsutomu Kawamura <cognitom@gmail.com>
2018-04-27 15:35:47 +0000

    Uglify for production

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,16 @@
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import uglify from 'rollup-plugin-uglify'
 
 const production = !process.env.ROLLUP_WATCH
 
 export default {
+  input: 'lib/index.js',
   plugins: [
-    resolve(),
-    commonjs()
+    production && uglify()
   ],
   output: {
+    file: 'index.js',
     format: 'umd',
+    name: 'dominiq',
     sourcemap: !production
   }
 }
