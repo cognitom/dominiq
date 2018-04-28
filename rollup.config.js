f796ad4db8ac5ce235422bd09f6fd20568ea0d32
Tsutomu Kawamura <cognitom@gmail.com>
2018-04-28 14:41:23 +0000

    Makes path absolute for Travis

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
+import {join} from "path"
 import uglify from "rollup-plugin-uglify"
 
 const production = !process.env.ROLLUP_WATCH
 
 export default {
-  input: "lib/index.js",
+  input: join(__dirname, "lib", "index.js"),
   plugins: [production && uglify()],
   output: {
-    file: "index.js",
+    file: join(__dirname, "index.js"),
     format: "umd",
     name: "dominiq",
     sourcemap: !production
