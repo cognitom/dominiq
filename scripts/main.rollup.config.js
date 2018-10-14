3d7ab93c6c19b3cf2fcf7046b5cb12990d8b004a
Tsutomu Kawamura <cognitom@gmail.com>
2018-10-14 10:18:26 +0000

    Removes the lines not used

--- scripts/main.rollup.config.js
+++ scripts/main.rollup.config.js
@@ -1,9 +1,7 @@
-import { join } from "path"
 import resolve from "rollup-plugin-node-resolve"
 import { uglify } from "./uglify.js"
 
 const production = !process.env.ROLLUP_WATCH
-const nodeModules = join(__dirname, "..", "node_modules")
 
 export default {
   input: "lib/index.js",
